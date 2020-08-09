54cf58fe48f8c278dbb60e497db41203a506f53e
Anton Mastiuk <anton.mastiuk@gmail.com>
2020-08-09 16:02:26 +0300

    init

---
+++ src/test/java/com/github/antonmastiuk/githubactivity/GithubActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonmastiuk.githubactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
